e53ce9cfbd659ca356fb6902d65088d2d3bf9c1f
ikebata_yuki <walkurepqrt@gmail.com>
2023-02-26 22:40:49 +0900

    環境変数の導入

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -9,7 +9,7 @@ const options = {
 
 const client = applyCaseMiddleware(
   axios.create({
-    baseURL: 'http://localhost:3001/api/v1',
+    baseURL: process.env.REACT_APP_API_URL,
   }),
   options
 );
