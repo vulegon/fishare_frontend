93b4995bd7302d16c81ee0750f13a78d2afb51ca
ikebata_yuki <walkurepqrt@gmail.com>
2023-02-28 08:13:33 +0900

    グーグルマップの表示完了

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import List from './components/List';
+import Map from './components/Map';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<List />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<List />} />
+        </Routes>
+      </BrowserRouter>
+      <Map className='Map'/>
+    </>
   );
 };
 export default App;
