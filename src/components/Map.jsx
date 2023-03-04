ca4198245137642c995eac30f4b62d14dbd5fc7a
ikebata_yuki <walkurepqrt@gmail.com>
2023-03-04 19:01:22 +0900

    クリックしたときにマーカーを設置する　完了

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,34 +1,41 @@
 // app/javascript/components/App.js
-import React, { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import React, { useState } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import { api_key } from '../config/api_key';
 
 
 function Map() {
   const mapStyles = { height: "100%", width: "100%" };
-  //福井県坂井市丸岡町
-  const defaultUserPosition = { lat: 36.15305354356379, lng: 136.2725972414738 };
-  const [userCurrentPosition, setUserCurrentPosition] = useState(defaultUserPosition)
+  const defaultUserPosition = { lat: 36.15305354356379, lng: 136.2725972414738 };   //丸岡城
+  const [userCurrentPosition, setUserCurrentPosition] = useState(defaultUserPosition);
+  const [marker, setMarker] = useState(defaultUserPosition);
   const zoom = 15;
-  const getCurrentPositionOptions={ timeout: 10000, maximumAge: 1500 }
+  const getCurrentPositionOptions = { timeout: 10000, maximumAge: 1500 }
 
   const getUserCurrentPosition = () => {
     const isUserDeviceCanGetCurrentPosition = navigator.geolocation
     if (isUserDeviceCanGetCurrentPosition) {
       isUserDeviceCanGetCurrentPosition.getCurrentPosition((position) => {
         setUserCurrentPosition({ lat: position.coords.latitude, lng: position.coords.longitude })
-      },(()=>{
+      }, (() => {
         console.log('位置情報を取得できませんでした。')
-      }),getCurrentPositionOptions)
+      }), getCurrentPositionOptions)
     }
   }
   useState(() => {
     getUserCurrentPosition();
   }, [])
 
+  const onMapClick = (e) => {
+    const clickPosition = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+    setMarker(clickPosition);
+  }
+
   return (
     <LoadScript googleMapsApiKey={api_key}>
-      <GoogleMap mapContainerStyle={mapStyles} zoom={zoom} center={userCurrentPosition} />
+      <GoogleMap mapContainerStyle={mapStyles} zoom={zoom} center={userCurrentPosition} onClick={onMapClick}>
+        <Marker position={{ lat: marker.lat, lng: marker.lng }} />
+      </GoogleMap>
     </LoadScript>
   )
 }
