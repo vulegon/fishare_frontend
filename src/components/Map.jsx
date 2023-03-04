c3b6359bb24a93b56b403fbb2428e26a6f12cf17
ikebata_yuki <walkurepqrt@gmail.com>
2023-03-04 14:43:49 +0900

    改行が多いのでリファクター

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,19 +5,12 @@ import { api_key } from '../config/api_key';
 
 
 function Map() {
-  const mapStyles = {
-    height: "100%",
-    width: "100%"
-  };
-  const defaultUserPosition = {
-    lat: 36.15305354356379, lng: 136.2725972414738 //福井県坂井市丸岡町
-  };
+  const mapStyles = { height: "100%", width: "100%" };
+  //福井県坂井市丸岡町
+  const defaultUserPosition = { lat: 36.15305354356379, lng: 136.2725972414738 };
   const [userCurrentPosition, setUserCurrentPosition] = useState(defaultUserPosition)
   const zoom = 15;
-  const getCurrentPositionOptions={
-    timeout: 10000,
-    maximumAge: 1500
-  }
+  const getCurrentPositionOptions={ timeout: 10000, maximumAge: 1500 }
 
   const getUserCurrentPosition = () => {
     const isUserDeviceCanGetCurrentPosition = navigator.geolocation
@@ -35,11 +28,7 @@ function Map() {
 
   return (
     <LoadScript googleMapsApiKey={api_key}>
-      <GoogleMap
-        mapContainerStyle={mapStyles}
-        zoom={zoom}
-        center={userCurrentPosition}
-      />
+      <GoogleMap mapContainerStyle={mapStyles} zoom={zoom} center={userCurrentPosition} />
     </LoadScript>
   )
 }
